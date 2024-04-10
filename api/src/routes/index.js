04f406466ed4d5aaa5b9b6b36d868c191ab354a5
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-04-10 16:01:44 -0300

    ROCKETSEAT-EXPLORER-STAGE10

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,20 +1,15 @@
-// import Router from express
 const { Router } = require("express");
 
-// a route
 const usersRouter = require("./users.routes.js");
 const notesRouter = require("./notes.routes.js");
 const tagsRouter = require("./tags.routes.js");
 const sessionRouter = require("./sessions.routes.js");
 
-// groups all routes
 const routes = Router();
 
-// (route, group)
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
 routes.use("/sessions", sessionRouter);
 
-// export routes
 module.exports = routes;
